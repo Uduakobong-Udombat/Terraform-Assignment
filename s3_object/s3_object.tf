5d4f07c2fcdc904c28089ba56e8835b252dbbdff
Uduakobong-Udombat <yuddieudombat@gmail.com>
2024-06-28 23:17:56 +0100

    Delete s3_object directory

--- s3_object/s3_object.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_object" "upload" {
-  for_each    = fileset("modules/s3_object/personal/", "**/*.*")
-  bucket      = "uduakudombat"
-  key         = each.value
-  source      = "modules/s3_object/personal/${each.value}"
-  content_type = "html"
-
-}
